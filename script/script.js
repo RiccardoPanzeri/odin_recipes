98eb598f15a2b463cb463091fb9025c0627c6dc5
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-02-02 02:12:25 +0100

    correzioni ai commenti

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 function mostraIngredienti(idElemento){
-    var listaIngredienti = document.getElementById(idElemento); //ottengo l'elemento immediatamente successivo al pulsante ( ovvero la lista), e lo inserisco in una variabile;
-    listaIngredienti.classList.toggle("nascondi"); //nascondo/mostro la classe ''nascondi";
-    console.log("chiamata");
+    var listaIngredienti = document.getElementById(idElemento); //ottengo l'elemento tramite l'id assegnatogli, passandolo come parametro al metodo getElementById;
+    listaIngredienti.classList.toggle("nascondi"); //attivo/disattivo la classe ''nascondi";
+    console.log("chiamata"); //log a scopo di debug e per testare la console tramite browser.
 
 }
